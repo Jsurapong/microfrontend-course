ad2199388ec1b29f8bf94f177c5d56ccf5d34741
surapong <surapong@enres.co>
2020-12-08 15:58:11 +0700

    added marketing deployment

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require("webpack-merge");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
@@ -21,6 +20,9 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
     }),
   ],
 };
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,14 +1,12 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
-const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+const commonConfig = require("./webpack.common");
 
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
